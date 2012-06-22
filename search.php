a3df1cf680ada33497bc878e47199c3b463c6f90
David Teter <cosmicshades@gmail.com>
2012-06-22 09:35:43 -0400

    Links completed, base layout complete

--- search.php
+++ search.php
@@ -3,6 +3,13 @@
 <head>
 </head>
 <body>
-<a href="index.html">Home</a> <a href="search.php">Job Search</a> <a href="job.html">Submit a Job</a> <a href="about.html">About</a>  
+<p>
+<form>
+Search: <input type="text" name="search" /> 
+<!-- begins on ENTER or Submit -->
+<input type="submit" name="Submit" />
+</form>
+<a href="index.html">Home</a> <a href="search.php">Search</a> <a href="job.html">Submit a Job</a> <a href="about.html">About</a>  
+</p>
 </body>
 </html>
